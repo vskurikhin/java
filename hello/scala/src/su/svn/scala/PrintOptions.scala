2b3ab1a5aea8c9af45baddfbdcaf4fb47bf17556
Victor N. Skurikhin <vskurikhin@gmail.com>
2019-12-02 00:08:20 +0300

    cleanup

--- hello/scala/src/su/svn/scala/PrintOptions.scala
+++
@@ -1,7 +0,0 @@
-package su.svn.scala
-
-object PrintOptions {
-  def main(args: Array[String]): Unit = {
-    println("Выбраны опции: " +: (args filter(_ startsWith "-") map(" " + _.drop(1))) mkString "\n")
-  }
-}
