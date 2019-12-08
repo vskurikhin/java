3319518f5729bc1e35ab93397d96ba5e97562b11
Victor N. Skurikhin <vskurikhin@gmail.com>
2019-12-08 22:55:27 +0300

    Java Полное руководство / Многопоточное программирование.

---
+++ complete-reference-10th-edt/chap11/src/main/java/su/svn/chap11/Producer.java
@@ -0,0 +1,20 @@
+package su.svn.chap11;
+
+public class Producer implements Runnable {
+    Q q;
+    Thread t;
+
+    Producer(Q q) {
+        this.q = q;
+        t = new Thread(this, "Producer");
+    }
+
+    public void run() {
+        int i = 0;
+
+        while (true) {
+            q.put(i++);
+        }
+    }
+}
+
